fe97f189821e92ffab8812fb59282fa115499ca1
carlosleonam <carlosleonam@users.noreply.github.com>
2016-08-08 15:54:00 -0300

    Update HTMLNestComments.py
    Hi,
    
    One suggestion: a small change to comment PHP that is inserted in HTML.
    
    Regards

--- HTMLNestComments.py
+++ HTMLNestComments.py
@@ -12,11 +12,13 @@ class NestCommand(sublime_plugin.TextCommand):
         text = text[:-3]
         text = text.replace('<!~~', '<!--')
         text = text.replace('~~>', '-->')
+        text = text.replace('<~?php','<?php')
         self.view.replace(edit, region, text)
       else:
         # comment the nested comments
         text = text.replace('<!--', '<!~~')
         text = text.replace('-->', '~~>')
+        text = text.replace('<?php','<~?php')
         self.view.replace(edit, region, text)
         self.view.insert(edit, self.view.sel()[0].begin(), "<!--")
         self.view.insert(edit, self.view.sel()[0].end(), "-->")
